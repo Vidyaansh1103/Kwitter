516ecd5cc48163b49537e9a3262c04b46998665d
Vidyaansh1103 <86597362+Vidyaansh1103@users.noreply.github.com>
2021-10-15 19:47:14 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,20 @@
+
+
+var firebaseConfig = {
+      apiKey: "AIzaSyAwaopOR4AWHxdwuzhC0VQcthyiFm5umHo",
+      authDomain: "kwitter-b4372.firebaseapp.com",
+      databaseURL: "https://kwitter-b4372.firebaseio.com",
+      projectId: "kwitter-b4372",
+      storageBucket: "kwitter-b4372.appspot.com",
+      messagingSenderId: "614813464395",
+      appId: "1:614813464395:web:bcac176aec5d757f575ace"
+    };
+    firebase.initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
